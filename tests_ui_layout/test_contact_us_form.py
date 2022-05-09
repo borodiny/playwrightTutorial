75293018505c6b7527b3cfb7beebcb7a4aeced42
yborodi <yevhen.borodin@gmail.com>
2022-05-09 16:43:56 +0300

    updating tcs7

--- tests_ui_layout/test_contact_us_form.py
+++ tests_ui_layout/test_contact_us_form.py
@@ -1,55 +1,55 @@
-import time
-
-import pytest
-from playwright.sync_api import Playwright, sync_playwright, expect
-
-from conftest import set_up
-from pom.contact_us_page import ContactUsPage
-
-# two or more markers could be added to the test
-@pytest.mark.smoke
-@pytest.mark.regression
-def test_submit_form(playwright: set_up):
-    browser = playwright.chromium.launch()
-    page = browser.new_page()
-    contact_us = ContactUsPage(page)
-    contact_us.navigate()
-    contact_us.submit_form("Yevhen", "11 street", "test@test.com", "1234567890", "subject", "message")
-
-
-# @pytest.mark.skip(reason="form is not ready")
-def test_submit_form2(playwright: set_up):
-    browser = playwright.chromium.launch()
-    page = browser.new_page()
-    contact_us = ContactUsPage(page)
-    contact_us.navigate()
-    contact_us.submit_form("Yevhen", "11 street", "test@test.com", "1234567890", "subject", "message")
-
-
+# import time
+#
+# import pytest
+# from playwright.sync_api import Playwright, sync_playwright, expect
+#
+# from conftest import set_up
+# from pom.contact_us_page import ContactUsPage
+#
+# # two or more markers could be added to the test
+# @pytest.mark.smoke
 # @pytest.mark.regression
-def test_submit_form3(playwright: set_up):
-    browser = playwright.chromium.launch()
-    page = browser.new_page()
-    contact_us = ContactUsPage(page)
-    contact_us.navigate()
-    contact_us.submit_form("Yevhen", "11 street", "test@test.com", "1234567890", "subject", "message")
-
-
-# @pytest.mark.xfail(reason="url is not ready")
-def test_run2(set_up) -> None:
-    page = set_up
-
-    # Act \ When/And
-    page.locator("p:has-text(\"Contact Us\")").click()
-    time.sleep(5)  # page timeout in seconds
-    page.locator("text=Log In").click()
-    page.locator("[data-testid=\"signUp\\.switchToSignUp\"]").click()
-    page.locator("[data-testid=\"switchToEmailLink\"] [data-testid=\"buttonElement\"]").click()
-    page.locator("[data-testid=\"emailAuth\"] input[type=\"email\"]").fill("test@test.com")
-    page.locator("[data-testid=\"emailAuth\"] input[type=\"email\"]").click()
-    page.locator("input[type=\"password\"]").fill("Test1234")
-    page.locator("[data-testid=\"submit\"] [data-testid=\"buttonElement\"]").click()
-    page.locator("[aria-label=\"test account menu\"]").click()
-
-    # Assertion / Then
-    assert page.is_visible("text= My Orders")
+# def test_submit_form(playwright: set_up):
+#     browser = playwright.chromium.launch()
+#     page = browser.new_page()
+#     contact_us = ContactUsPage(page)
+#     contact_us.navigate()
+#     contact_us.submit_form("Yevhen", "11 street", "test@test.com", "1234567890", "subject", "message")
+#
+#
+# # # @pytest.mark.skip(reason="form is not ready")
+# # def test_submit_form2(playwright: set_up):
+# #     browser = playwright.chromium.launch()
+# #     page = browser.new_page()
+# #     contact_us = ContactUsPage(page)
+# #     contact_us.navigate()
+# #     contact_us.submit_form("Yevhen", "11 street", "test@test.com", "1234567890", "subject", "message")
+#
+#
+# # # @pytest.mark.regression
+# # def test_submit_form3(playwright: set_up):
+# #     browser = playwright.chromium.launch()
+# #     page = browser.new_page()
+# #     contact_us = ContactUsPage(page)
+# #     contact_us.navigate()
+# #     contact_us.submit_form("Yevhen", "11 street", "test@test.com", "1234567890", "subject", "message")
+#
+#
+# # @pytest.mark.xfail(reason="url is not ready")
+# def test_run2(set_up) -> None:
+#     page = set_up
+#
+#     # Act \ When/And
+#     page.locator("p:has-text(\"Contact Us\")").click()
+#     time.sleep(5)  # page timeout in seconds
+#     page.locator("text=Log In").click()
+#     page.locator("[data-testid=\"signUp\\.switchToSignUp\"]").click()
+#     page.locator("[data-testid=\"switchToEmailLink\"] [data-testid=\"buttonElement\"]").click()
+#     page.locator("[data-testid=\"emailAuth\"] input[type=\"email\"]").fill("test@test.com")
+#     page.locator("[data-testid=\"emailAuth\"] input[type=\"email\"]").click()
+#     page.locator("input[type=\"password\"]").fill("Test1234")
+#     page.locator("[data-testid=\"submit\"] [data-testid=\"buttonElement\"]").click()
+#     page.locator("[aria-label=\"test account menu\"]").click()
+#
+#     # Assertion / Then
+#     assert page.is_visible("text= My Orders")
