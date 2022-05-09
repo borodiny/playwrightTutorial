ab488c3ba2c53360fa22fb9844a4740ce60b23b6
yborodi <yevhen.borodin@gmail.com>
2022-05-09 14:27:21 +0300

    updating tcs3

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 import pytest
 from playwright.sync_api import Playwright
 
+from pom.layout_selectors import playwright
+
 
 @pytest.fixture
 def set_up(page):
-    # # Assess - pre-requisites \ Given
-    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    # context = browser.new_context()
-    # # Open new page
-    # page = context.new_page()
+    # Assess - pre-requisites \ Given
+    browser = playwright.chromium.launch()
+    context = browser.new_context()
+    # Open new page
+    page = context.new_page()
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.set_default_timeout(5000)
 
